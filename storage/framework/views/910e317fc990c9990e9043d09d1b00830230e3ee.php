47d796f3afd6917051a0d558564466a31334cb4b
Eric Chiu <ericsonic520@hotmail.com>
2026-08-13 21:58:02 +0800

    更新

--- storage/framework/views/910e317fc990c9990e9043d09d1b00830230e3ee.php
+++ storage/framework/views/910e317fc990c9990e9043d09d1b00830230e3ee.php
@@ -77,7 +77,7 @@
 }
 </style>
 <!-- Footer -->
-<footer>
+<footer><?php dd($PostPaginate[0]->site_name); ?>
         <div class="text-center">
           <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
             <span class="glyphicon glyphicon-chevron-up"></span>
